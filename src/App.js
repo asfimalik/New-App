eb825780fc1cd9d67a0b63e7061f49c8aa40082c
Moqeet <asfimalik196@gmail.com>
2020-11-17 21:51:23 +0500

    update version

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Header from "./component/Header"
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit src/App.js and never try to learn reactjs
-        </p>
-        <a
-          className="App-link"
-          href="https://google.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-         Never try to reactive native
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <Header name="Moqeet"/>
+      </div>
+    );
+  }
+  }
+  
 
 export default App;
---
+++ src/component/Header.js
@@ -0,0 +1,9 @@
+import React from "react"
+//import ReactDOM from "react-dom"
+const Header = (Props) => {
+return (
+<p> Is my name {Props.name}</p>
+)
+}
+
+export default Header;
